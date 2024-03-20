88dfd8c84a75769e2386e362767e0948c5f826d1
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2024-03-20 20:50:10 +0300

    add navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../../images/logo.png'
+import logo from '../../images/logo2.png'
 import phone from '../../images/telephone.png'
 import facebook from '../../images/facebook.png'
 import email from '../../images/email.png'
@@ -12,23 +12,20 @@ const Navbar = () => {
        
         <img className="img" src={logo} alt=""/>
         <div>
-        
-<button className="button">Login</button> 
-<button className="button">Signup</button>
-<button className="button">Login</button> 
-<button className="button">Signup</button>
-<button className="button">Login</button> 
-<button className="button">Signup</button>
+<button className="button">Clothes</button> 
+<button className="button">About</button> 
+<button className="button">Signup</button>{/*if not logged in */}
+<button className="button">Login</button> {/*if not logged in */}
+ {/* <button className="button">Purchases</button> if logged in */}
+{/* <button className="button">Cart</button> if logged in */}
+{/* <button className="button">Profile</button>if logged in */}
 
 
 <a href="mailto:yabitekuam@gmail.com" title="Email">
     <img  className="icon" src={email} alt=""/>
+    </a>
     <a href="tel:+123456789" title="Call">
     <img  className="icon" src={phone} alt=""/>
-</a>
-        
-          
-        
 </a>
 
         </div>
