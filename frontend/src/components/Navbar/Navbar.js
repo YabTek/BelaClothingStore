3d61c8f4bcf23e80e30d24c4d1207155ef4bf16d
YabTek <yabtekuam@gmail.com>
2026-05-16 19:48:08 +0200

    navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,36 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../images/logo2.png'
 import phone from '../../images/telephone.png'
-import facebook from '../../images/facebook.png'
 import email from '../../images/email.png'
-import linkedin from '../../images/linkedin.png'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const handleNavClick = () => {
+    setIsMenuOpen(false)
+  }
+
   return (
-    <div className="navbar">
-       
-        <img className="img" src={logo} alt=""/>
-        <div>
-<button className="button">Clothes</button> 
-<button className="button">About</button> 
-<button className="button">Signup</button>{/*if not logged in */}
-<button className="button">Login</button> {/*if not logged in */}
- {/* <button className="button">Purchases</button> if logged in */}
-{/* <button className="button">Cart</button> if logged in */}
-{/* <button className="button">Profile</button>if logged in */}
-
-
-<a href="mailto:yabitekuam@gmail.com" title="Email">
-    <img  className="icon" src={email} alt=""/>
-    </a>
-    <a href="tel:+123456789" title="Call">
-    <img  className="icon" src={phone} alt=""/>
-</a>
-
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img src={logo} alt="Bela Clothing Store" className="logo-image" />
         </div>
-        
-    </div>
+
+        <div 
+          className={`hamburger ${isMenuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+          <a href="#clothes" className="nav-link" onClick={handleNavClick}>
+            Clothes
+          </a>
+          <a href="#about" className="nav-link" onClick={handleNavClick}>
+            About
+          </a>
+          <button className="nav-btn signup-btn" onClick={handleNavClick}>
+            Sign Up
+          </button>
+          <button className="nav-btn login-btn" onClick={handleNavClick}>
+            Login
+          </button>
+        </div>
+
+        <div className="contact-icons">
+          <a 
+            href="mailto:yabitekuam@gmail.com" 
+            title="Email" 
+            className="icon-link"
+          >
+            <img src={email} alt="Email" className="icon" />
+          </a>
+          <a 
+            href="tel:+123456789" 
+            title="Call" 
+            className="icon-link"
+          >
+            <img src={phone} alt="Phone" className="icon" />
+          </a>
+        </div>
+      </div>
+    </nav>
   )
 }
 
